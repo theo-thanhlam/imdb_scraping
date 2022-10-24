f360ed25c1745ef2fcd499962bbebeb200923e34
theo-thanhlam <phatlam.media.vis@gmail.com>
2022-10-24 16:22:32 -0400

    Scrap more charts

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-url = 'https://www.imdb.com/chart/top/'
+sites = ["boxoffice", "moviemeter", "top", "top-english-movies", "tvmeter", "toptv", "bottom"]
 
+url = 'https://www.imdb.com/chart/' 
+csv_name = "imdb_"
+
+
+    
 def get_years_list(year_tags):
     return [year.text.strip("()") for year in year_tags]
 
@@ -17,32 +22,43 @@ def get_rating_list(rating_tags):
     return [tag.text for tag in rating_tags]
 
 def export_to_csv(df, title):
-    return df.to_csv(title)
+    return df.to_csv(title)   
+
 
 def main():
-    #access URL and get html
-    response = requests.get(url)
-    html = response.text
-    
-    #get movies tag
-    soup = BeautifulSoup(html, "html.parser")
-    
-    #Tags that get movie titles, actors, years
-    movie_tag_for_title_actors = soup.select("td.titleColumn a")
-    movie_tag_for_years = soup.select("td.titleColumn span")
-    
-    #Tags that get movie rating
-    movie_rating_tags = soup.select("td.imdbRating strong")
-    
-    #Create dataframe with these value
-    df = pd.DataFrame({
-        "title": get_titles_list(movie_tag_for_title_actors),
-        "actor": get_actors_list(movie_tag_for_title_actors),
-        "year":get_years_list(movie_tag_for_years),
-        'rating':get_rating_list(movie_rating_tags)
-    })
-    
-    
+    for site in sites :
+       
+        url_link = url + site +"/"
+
+        file_name = csv_name + site +".csv"
+
+        print(url_link +"\n"+file_name)
+        #access URL and get html
+        response = requests.get(url_link)
+        html = response.text
+            
+            #get movies tag
+        soup = BeautifulSoup(html, "html.parser")
+            
+            #Tags that get movie titles, actors, years
+        movie_tag_for_title_actors = soup.select("td.titleColumn a")
+        movie_tag_for_years = soup.select("td.titleColumn span")
+            
+            #Tags that get movie rating
+        movie_rating_tags = soup.select("td.imdbRating strong")
+        
+        print("title:{}\tactor:{}\tyear:{}\trating:{}".format(len(get_titles_list(movie_tag_for_title_actors)), len(get_actors_list(movie_tag_for_title_actors)),
+                                                                  len(get_years_list(movie_tag_for_years)), len(get_rating_list(movie_rating_tags))))
+        
+        # #Create dataframe with these value
+        # df = pd.DataFrame({
+        #     "title": get_titles_list(movie_tag_for_title_actors),
+        #     "actor": get_actors_list(movie_tag_for_title_actors),
+        #     "year":get_years_list(movie_tag_for_years),
+        #     'rating':get_rating_list(movie_rating_tags)
+        # })
+        
+
     
 
 
